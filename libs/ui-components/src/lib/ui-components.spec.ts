e3a1bd772460528f3f1ddd02870cc5de5c462fd0
Jason Neal <jnea157@wgu.edu>
2025-08-23 00:35:57 -0400

    get rid of boilerplate

--- libs/ui-components/src/lib/ui-components.spec.ts
+++ libs/ui-components/src/lib/ui-components.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UiComponents } from './ui-components';
 
-describe('UiComponents', () => {
+describe(UiComponents.name, () => {
   let component: UiComponents;
   let fixture: ComponentFixture<UiComponents>;
 
