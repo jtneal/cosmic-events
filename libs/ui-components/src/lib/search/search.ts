71db3459fdc4680b38fced9cdf7343d7e3103019
Jason Neal <jnea157@wgu.edu>
2025-08-28 22:16:14 -0400

    search done

--- libs/ui-components/src/lib/search/search.ts
+++ libs/ui-components/src/lib/search/search.ts
@@ -6,29 +6,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
-  imports: [
-    MatChipsModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatSelectModule,
-    MatSliderModule,
-  ],
+  imports: [MatChipsModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule],
   providers: [provideNativeDateAdapter()],
   selector: 'lib-search',
   styleUrl: './search.scss',
   templateUrl: './search.html',
 })
-export class Search {
-  public formatSliderLabel(value: number): string {
-    if (value >= 1000) {
-      return `${Math.round(value / 1000)}k`;
-    }
-
-    return `${value}`;
-  }
-}
+export class Search {}
