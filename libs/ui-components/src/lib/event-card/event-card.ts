6c6641d4b1822b0b2b1f20f3fda4701bb9a0222f
Jason Neal <jnea157@wgu.edu>
2025-08-29 21:24:37 -0400

    fix card styles

--- libs/ui-components/src/lib/event-card/event-card.ts
+++ libs/ui-components/src/lib/event-card/event-card.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatChipsModule, MatIconModule],
   selector: 'lib-event-card',
   styleUrl: './event-card.scss',
   templateUrl: './event-card.html',
