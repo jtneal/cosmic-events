0899b0accf6489440eed595f572e60fb27257db2
Jason Neal <jnea157@wgu.edu>
2025-09-09 21:44:36 -0400

    Search from a ui perspective is working

--- libs/feature-search/src/lib/search-feature.ts
+++ libs/feature-search/src/lib/search-feature.ts
@@ -1,5 +1,7 @@
 import { httpResource } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -9,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { EventCard, EventCardSkeleton, Hero } from '@cosmic-events/ui-components';
 import { EventDto } from '@cosmic-events/util-dtos';
+import { debounceTime } from 'rxjs';
 
 @Component({
   imports: [
@@ -21,18 +24,41 @@ import { EventDto } from '@cosmic-events/util-dtos';
     MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [provideNativeDateAdapter()],
   selector: 'lib-search-feature',
   styleUrl: './search-feature.scss',
   templateUrl: './search-feature.html',
 })
-export class SearchFeature {
-  public events = httpResource<EventDto[]>(() => '/api/events');
+export class SearchFeature implements OnInit {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly queryParams = signal('');
+
+  public readonly events = httpResource<EventDto[]>(() => `/api/events${this.queryParams()}`);
   public isHeroHidden = localStorage.getItem('isHeroHidden') === 'true';
+
+  public readonly form = this.formBuilder.group({
+    location: [''],
+    startDate: [''],
+    endDate: [''],
+    search: [''],
+    type: [''],
+    sort: [''],
+  });
+
+  private readonly formChange$ = this.form.valueChanges.pipe(debounceTime(250), takeUntilDestroyed());
+
+  public ngOnInit(): void {
+    this.formChange$.subscribe((formValue) => this.setQueryParams(formValue as Record<string, string>));
+  }
 
   public hideHero(): void {
     this.isHeroHidden = true;
     localStorage.setItem('isHeroHidden', 'true');
+  }
+
+  private setQueryParams(formValue: Record<string, string>): void {
+    this.queryParams.set(`?${new URLSearchParams(formValue).toString()}`);
   }
 }
