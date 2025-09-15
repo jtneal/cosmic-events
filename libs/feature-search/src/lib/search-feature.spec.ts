2f782f9db14558ac5fbdb9745b9d45a87daf2d25
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-15 00:53:30 -0700

    Fix search

--- libs/feature-search/src/lib/search-feature.spec.ts
+++ libs/feature-search/src/lib/search-feature.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { SearchFeature } from './search-feature';
 
 describe(SearchFeature.name, () => {
@@ -8,6 +10,16 @@ describe(SearchFeature.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SearchFeature],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+        {
+          provide: HttpClient,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchFeature);
