9063d549fe48fe480c6443167e369608da1b9112
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-15 00:51:45 -0700

    Fix report tests

--- libs/feature-report/src/lib/report-feature.spec.ts
+++ libs/feature-report/src/lib/report-feature.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { ReportFeature } from './report-feature';
 
 describe(ReportFeature.name, () => {
@@ -8,6 +10,12 @@ describe(ReportFeature.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReportFeature],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: { request: jest.fn(() => of()) },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReportFeature);
