7316b4abb738f50632a05a0611ecfa89f11f8ec9
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-15 20:47:32 -0700

    Add secrets for eks

--- iac/nonprod/eks.tf
+++ iac/nonprod/eks.tf
@@ -5,8 +5,27 @@ module "eks" {
   name               = "nonprod-cosmic-events-eks"
   kubernetes_version = "1.33"
 
+  authentication_mode                      = "API_AND_CONFIG_MAP"
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    console-admin = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+
+      policy_associations = {
+        admin = {
+          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = { type = "cluster" }
+        }
+      }
+    }
+  }
+
+  encryption_config = {
+    resources        = ["secrets"]
+    provider_key_arn = aws_kms_key.eks_secrets_key.arn
+  }
 
   compute_config = {
     enabled    = true
@@ -20,4 +39,38 @@ module "eks" {
     Environment = "nonprod"
     Terraform   = "true"
   }
+}
+
+resource "aws_kms_key" "eks_secrets_key" {
+  description             = "KMS key for EKS secrets encryption"
+  deletion_window_in_days = 7
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable KMS key usage for EKS"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow EKS service to use KMS key"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
