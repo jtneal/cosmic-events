7d0eb5c1722bf5676a861cf673959294e1786456
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-17 18:31:09 -0700

    Try to deploy from gha

--- iac/nonprod/ecs.tf
+++ iac/nonprod/ecs.tf
@@ -224,7 +224,7 @@ resource "aws_ecs_task_definition" "service" {
   container_definitions = jsonencode([
     {
       name        = "${local.name}-api"
-      image       = "186050465172.dkr.ecr.us-east-2.amazonaws.com/nonprod-cosmic-events-ecr:529a34b"
+      image       = "186050465172.dkr.ecr.us-east-2.amazonaws.com/nonprod-cosmic-events-ecr:IMAGE_TAG"
       cpu         = 256
       memory      = 512
       essential   = true
