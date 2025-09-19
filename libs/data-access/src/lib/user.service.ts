34c317e0aced2bdcc729b9ffac7834d5c23c6c66
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-19 13:49:43 -0700

    fix user share replay

--- libs/data-access/src/lib/user.service.ts
+++ libs/data-access/src/lib/user.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { UserDto } from '@cosmic-events/util-dtos';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private cache$?: Observable<UserDto>;
   private readonly http = inject(HttpClient);
 
   public getUser(): Observable<UserDto> {
-    return this.http.get<UserDto>('/api/user');
+    if (!this.cache$) {
+      this.cache$ = this.http.get<UserDto>('/api/user').pipe(shareReplay(1));
+    }
+
+    return this.cache$;
   }
 }
