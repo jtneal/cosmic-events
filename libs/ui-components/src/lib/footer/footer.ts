704b8bc4ad1e256a6d81bd04c9428ddd212659b0
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-09-19 13:28:49 -0700

    stop showing footer links when not logged in

--- libs/ui-components/src/lib/footer/footer.ts
+++ libs/ui-components/src/lib/footer/footer.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { UserDto } from '@cosmic-events/util-dtos';
 
 @Component({
   imports: [MatButtonModule, MatIconModule, RouterModule],
@@ -10,5 +11,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './footer.html',
 })
 export class Footer {
+  public user = input.required<UserDto | null>();
   public year = new Date().getFullYear();
 }
